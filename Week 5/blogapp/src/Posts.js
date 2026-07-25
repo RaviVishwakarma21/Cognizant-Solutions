9011839bc507690b90b32ea9149ad283ac661b71
Ravi Vishwakarma <ravivishwakarma16181@gmail.com>
2026-07-25 10:04:57 +0530

    Add files via upload

---
+++ Week 5/blogapp/src/Posts.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+class Posts extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    posts: data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        alert(error);
+        console.log(info);
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.posts.map(post => (
+                        <div key={post.id}>
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                            <hr />
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default Posts;
